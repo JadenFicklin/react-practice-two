05c62ddc9dc8bafa5a2ff6dec063ea33ca7f8f5d
JadenFicklin <jadendficklin@gmail.com>
2022-02-23 19:21:29 -0700

    useState hooks

--- hooks/usestate/src/App.js
+++ hooks/usestate/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import ClassCounter from "./components/ClassCounter";
 import HookCounter from "./components/HookCounter";
+import HookCounterTwo from "./components/HookCounterTwo";
 
 function App() {
   return (
     <div>
       <ClassCounter />
       <HookCounter />
+      <HookCounterTwo />
     </div>
   );
 }
